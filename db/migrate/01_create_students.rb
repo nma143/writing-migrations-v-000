06905c9a764f0a0b45249db3f4af3d2407f91e07
Nikki A <nikki.apostolakis@gmail.com>
2018-08-12 16:39:02 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,9 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+
+  def change #an alternative to up and down; change is more common for basic migrations
+     create_table :students do |t|
+       t.string :name
+     end
+   end
+
 end
